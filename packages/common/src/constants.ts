208fbb9b322f16f44879e975af240344ca74d277
AdiPol1359 <adrian.polak20@gmail.com>
2022-09-01 15:09:24 +0200

    refactor: typo

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -3,7 +3,7 @@ export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const USERNAME_REGEX = /^[a-z0-9\s]+$/;
 export const PASSWORD_REGEX = /^.{6,16}$/;
 
-export const FULL_NAME_ERROR_MESSAGE = 'Full name can only contain letters and numbers.';
+export const FULL_NAME_ERROR_MESSAGE = 'Full name can only contain letters.';
 export const EMAIL_ERROR_MESSAGE = 'Incorrect email.';
 export const USERNAME_ERROR_MESSAGE = 'Username can only contain lowercase letters and numbers.';
 export const PASSWORD_ERROR_MESSAGE = 'Password can be 6 to 16 characters long.';
--- packages/frontend/contants.ts
+++ packages/frontend/contants.ts
@@ -3,7 +3,7 @@ export const REGISTER_PAGE_PATH = '/sign-up';
 
 export const SERVER_ERROR_MESSAGE = 'Wystąpił problem z serwerem!';
 export const REQUIRED_FIELD_ERROR_MESSAGE = 'Uzupełnij to pole';
-export const FULL_NAME_ERROR_MESSAGE = 'Imie może zawierać jedynie litery oraz cyfry';
+export const FULL_NAME_ERROR_MESSAGE = 'Imie może zawierać jedynie litery';
 export const USERNAME_ERROR_MESSAGE = 'Nazwa użytkownika może zawierać jedynie małe litery oraz cyfry';
 export const EMAIL_ERROR_MESSAGE = 'Wpisz poprawny adres e-mail';
 export const PASSWORD_ERROR_MESSAGE = 'Hasło musia zawierać od 6 do 16 znaków';
